9ff07c485b9b9127f4a37ef85e8ca1a2706a2f5c
xiyuan_fengyu <xiyuan_fengyu@163.com>
2017-07-20 15:53:01 +0800

    增加RandomByKeyPriorityQueue

---
+++ src/main/java/com/xiyuan/spider/queue/RandomByKeyPriorityQueue.java
@@ -0,0 +1,17 @@
+package com.xiyuan.spider.queue;
+
+import com.xiyuan.spider.message.Message;
+
+/**
+ * Created by xiyuan_fengyu on 2017/2/28.
+ */
+public class RandomByKeyPriorityQueue extends DefaultPriorityQueue {
+
+    private static final long serialVersionUID = -7547015624262181312L;
+
+    @Override
+    void computePriority(Message msg) {
+        msg.setPriority((int) (Math.random() * msg.key().length() * 100));
+    }
+
+}
